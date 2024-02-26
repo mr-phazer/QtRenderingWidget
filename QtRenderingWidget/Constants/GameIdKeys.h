2e5ba080b385f075d10e336d5b987b8d554a60be
mr-phazer <phazer1980@msn.com>
2024-02-26 21:59:17 +0100

    Changed Game IDs to WIDE char, for use with std::wstring

--- QtRenderingWidget/Constants/GameIdKeys.h
+++ QtRenderingWidget/Constants/GameIdKeys.h
@@ -2,18 +2,18 @@
 #include <string>
 
 namespace game_id_keys
-{	
-	static constexpr auto KEY_PHARAOH = "pharaoh";                            // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_WARHAMMER_3 = "warhammer_3";                    // Filtered and revised incorrect AK fields.
-	static constexpr auto KEY_TROY = "troy";                                  // Filtered and revised incorrect AK fields.
-	static constexpr auto KEY_THREE_KINGDOMS = "three_kingdoms";              // Filtered and revised incorrect AK fields.
-	static constexpr auto KEY_WARHAMMER_2 = "warhammer_2";                    // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_WARHAMMER = "warhammer";                        // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_THRONES_OF_BRITANNIA = "thrones_of_britannia";  // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_ATTILA = "attila";                              // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_ROME_2 = "rome_2";                              // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_SHOGUN_2 = "shogun_2";                          // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_NAPOLEON = "napoleon";                          // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_EMPIRE = "empire";                              // Pending of schema review for incorrect AK fields.
-	static constexpr auto KEY_ARENA = "arena";
-}
+{
+	static constexpr auto KEY_PHARAOH = L"pharaoh";                            // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_WARHAMMER_3 = L"warhammer_3";                    // Filtered and revised incorrect AK fields.
+	static constexpr auto KEY_TROY = L"troy";                                  // Filtered and revised incorrect AK fields.
+	static constexpr auto KEY_THREE_KINGDOMS = L"three_kingdoms";              // Filtered and revised incorrect AK fields.
+	static constexpr auto KEY_WARHAMMER_2 = L"warhammer_2";                    // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_WARHAMMER = L"warhammer";                        // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_THRONES_OF_BRITANNIA = L"thrones_of_britannia";  // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_ATTILA = L"attila";                              // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_ROME_2 = L"rome_2";                              // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_SHOGUN_2 = L"shogun_2";                          // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_NAPOLEON = L"napoleon";                          // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_EMPIRE = L"empire";                              // Pending of schema review for incorrect AK fields.
+	static constexpr auto KEY_ARENA = L"arena";
+};
