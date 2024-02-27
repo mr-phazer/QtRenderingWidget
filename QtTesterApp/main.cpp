1c44cad599ad80fa3c9432a7b942e8305460c1d4
mr-phazer <phazer1980@msn.com>
2024-02-27 02:49:49 +0100

    Very Minor Edit

--- QtTesterApp/main.cpp
+++ QtTesterApp/main.cpp
@@ -1,14 +1,14 @@
-#include "QtMainWindowView.h"
 #include <QtWidgets/QApplication>
+#include "QtTestAppView.h"
 
 #include "..\ImportExport\RigidModel\Readers\RigidModelReader.h"
 
-int main(int argc, char *argv[])
+int main(int argc, char* argv[])
 {
-    QApplication a(argc, argv); 
+	QApplication a(argc, argv);
 
-    QtMainWindowView w;
-    w.show();
+	QtMainWindowView w;
+	w.show();
 
-    return a.exec();
+	return a.exec();
 }
