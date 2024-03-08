a4871add9f142c0c99913b7d5bcfe5bf2476542e
mr-phazer <phazer1980@msn.com>
2024-03-08 01:10:19 +0100

    WIP Skeleton "stick dummy" mesh

--- Rldx/Rldx/Helpers/DxMeshCreatorHelper.h
+++ Rldx/Rldx/Helpers/DxMeshCreatorHelper.h
@@ -14,4 +14,48 @@ namespace rldx
 		static DxCommonMeshData CreateFromRmv2Mesh(ID3D11Device* poDevice, const rmv2::MeshBlockCommon& rmv2Mesh);
 		static DxCommonMeshData CreateSkeletonMesh(ID3D11Device* poDevice, const skel_anim::Skeleton& animFileSkeleton);
 	};
+
+	class DxSkeletonMeshCreator
+	{
+	public:
+		DxCommonMeshData Create(ID3D11Device* poDevice, const skel_anim::Skeleton& animFileSkeleton)
+		{
+			std::vector<uint32_t> indices;
+			std::vector<CommonVertex> vertices;
+			FillLineIndices(animFileSkeleton, indices);
+			FillLineVertices(animFileSkeleton, vertices);
+
+			//TODO: remove to set mesh to "line list" (should it be called "mesh" then?, maybe rename to "CommonPrimitive"?)
+		}
+
+	private:
+		void FillLineIndices(const skel_anim::Skeleton& skeleton, std::vector<uint32_t>& indices)
+		{
+			for (const auto& itBone : skeleton.boneTable)
+			{
+				// line from parent to child
+				indices.push_back(itBone.parentIndex);
+				indices.push_back(itBone.boneIndex);
+
+			}
+		}
+
+		void FillLineVertices(const skel_anim::Skeleton& skeleton, std::vector<CommonVertex>& vertuces)
+		{
+			for (const auto& itBone : skeleton.boneTable)
+			{
+
+
+				/*
+				ALGO:
+					- finish/use FrameGenerator to generate bind pose matrices
+					- for every parent-child bone pair, tranform {0,0,0} by bone global tranform matrix
+
+					this ought to fint with line indices made above
+
+				*/
+
+			}
+		}
+	};
 }
