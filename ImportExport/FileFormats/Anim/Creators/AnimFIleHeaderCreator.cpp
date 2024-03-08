e1a62853620a75bda804022351322bb89b9ba5de
mr-phazer <phazer1980@msn.com>
2024-03-08 01:07:24 +0100

    Minor Edits

--- ImportExport/FileFormats/Anim/Creators/AnimFIleHeaderCreator.cpp
+++ ImportExport/FileFormats/Anim/Creators/AnimFIleHeaderCreator.cpp
@@ -1,36 +1,39 @@
 
 #include "AnimFIleHeaderCreator.h"
 
-anim_file::AnimHeaderCommon Anim_V6_HeaderFileCommonCreator::Create(ByteStream& bytes)
+namespace anim_file
 {
-	anim_file::AnimHeaderCommon in;
-	in.dwVersion = bytes.TReadElement<uint32_t>();
-	in.dwUnknown = bytes.TReadElement<uint32_t>();
-	in.fFrameRate = bytes.TReadElement<float>();
-	in.skeletonName = bytes.ReadLengthPrefixed16StringA();
-	in.fEndTime = bytes.TReadElement<float>();
-	in.dwBoneCount = bytes.TReadElement<uint32_t>();
-
-	return in;
-}
-
-anim_file::AnimHeaderCommon Anim_V7_HeaderFileCommonCreator::Create(ByteStream& bytes)
-{
-	anim_file::AnimHeaderCommon in;
-	in.dwVersion = bytes.TReadElement<uint32_t>();
-	in.dwUnknown = bytes.TReadElement<uint32_t>();
-	in.fFrameRate = bytes.TReadElement<float>();
-	in.skeletonName = bytes.ReadLengthPrefixed16StringA();
-
-	in.subHeader_V7.flagCount = bytes.TReadElement<uint32_t>();
-
-	if (in.subHeader_V7.flagCount != 00) // is there sub header?
+	anim_file::AnimHeaderCommon Anim_V6_HeaderFileCommonCreator::Create(ByteStream& bytes)
 	{
-		in.subHeader_V7.strText = bytes.TReadElement<uint32_t>();
+		anim_file::AnimHeaderCommon in;
+		in.dwVersion = bytes.TReadElement<uint32_t>();
+		in.dwUnknown = bytes.TReadElement<uint32_t>();
+		in.fFrameRate = bytes.TReadElement<float>();
+		in.skeletonName = bytes.ReadLengthPrefixed16StringA();
+		in.fEndTime = bytes.TReadElement<float>();
+		in.dwBoneCount = bytes.TReadElement<uint32_t>();
+
+		return in;
 	}
 
-	in.fEndTime = bytes.TReadElement<float>();
-	in.dwBoneCount = bytes.TReadElement<uint32_t>();
+	anim_file::AnimHeaderCommon Anim_V7_HeaderFileCommonCreator::Create(ByteStream& bytes)
+	{
+		anim_file::AnimHeaderCommon in;
+		in.dwVersion = bytes.TReadElement<uint32_t>();
+		in.dwUnknown = bytes.TReadElement<uint32_t>();
+		in.fFrameRate = bytes.TReadElement<float>();
+		in.skeletonName = bytes.ReadLengthPrefixed16StringA();
 
-	return in;
+		in.subHeader_V7.flagCount = bytes.TReadElement<uint32_t>();
+
+		if (in.subHeader_V7.flagCount != 00) // is there sub header?
+		{
+			in.subHeader_V7.strText = bytes.TReadElement<uint32_t>();
+		}
+
+		in.fEndTime = bytes.TReadElement<float>();
+		in.dwBoneCount = bytes.TReadElement<uint32_t>();
+
+		return in;
+	}
 }
--- ImportExport/FileFormats/Anim/Creators/AnimFIleHeaderCreator.h
+++ ImportExport/FileFormats/Anim/Creators/AnimFIleHeaderCreator.h
@@ -3,17 +3,20 @@
 #include "..\..\..\Helpers\Templates.h"
 #include "..\Types\Common\FileHeader.h"
 
-class IAnimHeaderCommonCreator : public  templates::TCreator <anim_file::AnimHeaderCommon> {};
-
-class Anim_V6_HeaderFileCommonCreator : public IAnimHeaderCommonCreator
+namespace anim_file
 {
-public:
-	virtual anim_file::AnimHeaderCommon Create(ByteStream& bytes) override;
-};
+	// TODO: maybe just make all this methods in TWAnimReader, like: AnimHeaderCommon ReadHeaderV7(), etc
+	class IAnimHeaderCommonCreator : public  templates::TCreator <anim_file::AnimHeaderCommon> {};
 
-class Anim_V7_HeaderFileCommonCreator : public IAnimHeaderCommonCreator
-{
-public:
-	virtual anim_file::AnimHeaderCommon Create(ByteStream& bytes) override;
-};
+	class Anim_V6_HeaderFileCommonCreator : public IAnimHeaderCommonCreator
+	{
+	public:
+		virtual anim_file::AnimHeaderCommon Create(ByteStream& bytes) override;
+	};
 
+	class Anim_V7_HeaderFileCommonCreator : public IAnimHeaderCommonCreator
+	{
+	public:
+		virtual anim_file::AnimHeaderCommon Create(ByteStream& bytes) override;
+	};
+}
