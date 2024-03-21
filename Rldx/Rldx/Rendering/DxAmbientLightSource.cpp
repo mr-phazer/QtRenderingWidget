48f71251a0c13fcaa39924b603f3844978b3f6fb
mr-phazer <phazer1980@msn.com>
2024-03-21 20:06:09 +0100

    Edits to "Ambient Light Sourtce"

--- Rldx/Rldx/Rendering/DxAmbientLightSource.cpp
+++ Rldx/Rldx/Rendering/DxAmbientLightSource.cpp
@@ -1,113 +1,106 @@
 #include "DxAmbientLightSource.h"
 
-
+#include "..\..\..\DXUT\Core\DXUTmisc.h"
+#include "..\..\ImportExport\Helpers\ByteStream.h"
 #include "..\Managers\ResourceManager\DxResourceManager.h"
 #include "..\Rendering\DxTexture.h"
-#include "..\..\ImportExport\Helpers\ByteStream.h"
-#include "..\..\..\DXUT\Core\DXUTmisc.h"
 
-using namespace rldx;
-
-DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, const std::wstring& pathDiffuseMap, const std::wstring& pathSpecularMap, const std::wstring& pathLUT, UINT startSlotSRV, UINT startSlotConstBuf)
+namespace rldx
 {
-	DxAmbientLightSource oNewInstance;
+	DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, const std::wstring& pathDiffuseMap,
+													  const std::wstring& pathSpecularMap, const std::wstring& pathLUT,
+													  UINT startSlotSrv, UINT startSlotConstBuf)
+	{
+		DxAmbientLightSource oNewInstance;
 
-	oNewInstance.SetTexturesFromFiles(poDevice, pathDiffuseMap, pathSpecularMap, pathLUT, startSlotSRV);
+		oNewInstance.SetTexturesFromFiles(poDevice, pathDiffuseMap, pathSpecularMap, pathLUT, startSlotSrv);
 
-	oNewInstance.m_oPSConstBuffer.buffer.Create(poDevice);
-	DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.buffer.GetBuffer(), psConstBufferName);
+		oNewInstance.m_oPSConstBuffer.Init(poDevice, "PS_AmbientLight_CB");
+		DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.GetBuffer(), psConstBufferName);
 
-	return oNewInstance;
-}
+		return oNewInstance;
+	}
 
-DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, DxTexture* diffuseCubemap, DxTexture* specularCubmap, DxTexture* lut, UINT startSlotConstBuf, UINT startSlotSRV)
-{
-	DxAmbientLightSource oNewInstance;
+	DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, DxTexture* diffuseCubemap,
+													  DxTexture* specularCubmap, DxTexture* lut, UINT startSlotConstBuf,
+													  UINT startSlotSRV)
+	{
+		DxAmbientLightSource oNewInstance;
 
-	oNewInstance.m_poDiffuseMap = diffuseCubemap;
-	oNewInstance.m_poSpecularMap = specularCubmap;
+		oNewInstance.m_poDiffuseMap = diffuseCubemap;
+		oNewInstance.m_poSpecularMap = specularCubmap;
 
-	oNewInstance.m_oPSConstBuffer.buffer.Create(poDevice);
-	DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.buffer.GetBuffer(), psConstBufferName);
+		oNewInstance.m_oPSConstBuffer.Init(poDevice, "PS_AmbientLight_CB");
+		DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.GetBuffer(), psConstBufferName);
 
-	return oNewInstance;
-}
+		return oNewInstance;
+	}
 
-DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, ByteStream& pathDiffuseMap, ByteStream& pathSpecularMap, ByteStream& pathLUT, UINT startSlotSRV, UINT startSlotConstBuf)
-{
-	DxAmbientLightSource oNewInstance;
+	DxAmbientLightSource DxAmbientLightSource::Create(ID3D11Device* poDevice, ByteStream& pathDiffuseMap,
+													  ByteStream& pathSpecularMap, ByteStream& pathLUT, UINT startSlotSRV,
+													  UINT startSlotConstBuf)
+	{
+		DxAmbientLightSource oNewInstance;
 
-	oNewInstance.SetTexturesFromMemory(poDevice, pathDiffuseMap, pathSpecularMap, pathLUT, startSlotSRV);
+		oNewInstance.SetTexturesFromMemory(poDevice, pathDiffuseMap, pathSpecularMap, pathLUT, startSlotSRV);
 
-	oNewInstance.m_oPSConstBuffer.buffer.Create(poDevice);
-	DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.buffer.GetBuffer(), psConstBufferName);
+		oNewInstance.m_oPSConstBuffer.Init(poDevice, "PS_DxAmbientLight_CB");
+		DXUT_SetDebugName(oNewInstance.m_oPSConstBuffer.GetBuffer(), psConstBufferName);
 
-	return oNewInstance;
-}
+		return oNewInstance;
+	}
 
-void DxAmbientLightSource::SetTexturesFromFiles(
-	ID3D11Device* poDevice,
-	const std::wstring& pathDiffuseMap,
-	const std::wstring& pathSpecularMap,
-	const std::wstring& pathLUT,
-	UINT startSlotSRV)
-{
-	if (pathDiffuseMap.empty())
-		throw invalid_argument("path for IBL Diffuse cubemap is empty");
+	void DxAmbientLightSource::SetTexturesFromFiles(ID3D11Device* poDevice, const std::wstring& pathDiffuseMap,
+													const std::wstring& pathSpecularMap, const std::wstring& pathLUT,
+													UINT startSlotSRV)
+	{
+		if (pathDiffuseMap.empty())
+			throw invalid_argument("path for IBL Diffuse cubemap is empty");
 
-	if (pathSpecularMap.empty())
-		throw invalid_argument("path for IBL Specular cubemap is empty");
+		if (pathSpecularMap.empty())
+			throw invalid_argument("path for IBL Specular cubemap is empty");
 
-	m_poDiffuseMap->LoadFileFromDisk(poDevice, pathDiffuseMap);
-	m_poSpecularMap->LoadFileFromDisk(poDevice, pathSpecularMap);
+		m_poDiffuseMap->LoadFileFromDisk(poDevice, pathDiffuseMap);
+		m_poSpecularMap->LoadFileFromDisk(poDevice, pathSpecularMap);
 
-	// TODO: if needed make an impl thata loads an LUT
-	/*if (!pathLUT.empty())
-		m_poLUT->LoadFileFromDisk(poDevice, pathLUT);*/
-}
+		// TODO: if needed make an impl thata loads an LUT
+		/*if (!pathLUT.empty())
+			m_poLUT->LoadFileFromDisk(poDevice, pathLUT);*/
+	}
 
-void DxAmbientLightSource::SetTexturesFromMemory(
-	ID3D11Device* poDevice,
-	ByteStream& streamDiffuseMap,
-	ByteStream& streamSpecularMap,
-	ByteStream& pathLUT,
-	UINT startSlotSRV)
-{
-	m_poDiffuseMap =
-		DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"DiffuseIBL").GetPtr();;
-	m_poSpecularMap =
-		DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"SpecularIBL").GetPtr();;
+	void DxAmbientLightSource::SetTexturesFromMemory(ID3D11Device* poDevice, ByteStream& streamDiffuseMap,
+													 ByteStream& streamSpecularMap, ByteStream& pathLUT, UINT startSlotSRV)
+	{
+		m_poDiffuseMap = DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"DiffuseIBL").GetPtr();
+		;
+		m_poSpecularMap = DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"SpecularIBL").GetPtr();
+		;
 
-	// TODO: this is used in some shaders so find a way to condintionally load it in neat way
-	//m_poLUT =
-	//	DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"LUT").GetPtr();;
+		// TODO: this is used in some shaders so find a way to condintionally load it in neat way
+		// m_poLUT =
+		//	DxResourceManager::Instance()->AllocEmpty<DxTexture>(L"LUT").GetPtr();;
 
-	m_poSpecularMap->LoadCubeMap(
-		poDevice,
-		streamSpecularMap.GetBufferPtr(),
-		streamSpecularMap.GetBufferSize(),
-		"IBL CubeMap Specular");
+		m_poSpecularMap->LoadCubeMap(poDevice, streamSpecularMap.GetBufferPtr(), streamSpecularMap.GetBufferSize(),
+									 "IBL CubeMap Specular");
 
-	m_poDiffuseMap->LoadCubeMap(
-		poDevice,
-		streamDiffuseMap.GetBufferPtr(),
-		streamDiffuseMap.GetBufferSize(),
-		"IBL CubeMcap Diffuse");
-}
+		m_poDiffuseMap->LoadCubeMap(poDevice, streamDiffuseMap.GetBufferPtr(), streamDiffuseMap.GetBufferSize(),
+									"IBL CubeMcap Diffuse");
+	}
 
-void DxAmbientLightSource::BindToDC(ID3D11DeviceContext* poDC)
-{		
-	m_oPSConstBuffer.SetStartSlot(0);
-	m_oPSConstBuffer.BindToDC(poDC);
-		
-	ID3D11ShaderResourceView* ppShaderResourceViews0[] = {
-		m_poDiffuseMap->GetShaderResourceView(),		
+	void DxAmbientLightSource::BindToDC(ID3D11DeviceContext* poDC)
+	{
+		m_oPSConstBuffer.SetStartSlot(0);
+		m_oPSConstBuffer.BindToDC(poDC);
+
+		ID3D11ShaderResourceView* pSrvDiffuseCubeMap[] = {
+			m_poDiffuseMap->GetShaderResourceView(),
+		};
+
+		ID3D11ShaderResourceView* pSrvSpecularMap[] = {
+			m_poSpecularMap->GetShaderResourceView(),
+		};
+
+		poDC->PSSetShaderResources(0, 1, pSrvDiffuseCubeMap);
+		poDC->PSSetShaderResources(1, 1, pSrvSpecularMap);
 	};
-
-	ID3D11ShaderResourceView* ppShaderResourceViews1[] = {
-		m_poSpecularMap->GetShaderResourceView(),		
-	};
-
-	poDC->PSSetShaderResources(0, 1, ppShaderResourceViews0);
-	poDC->PSSetShaderResources(1, 1, ppShaderResourceViews1);
-};
+} // namespace rldx
--- Rldx/Rldx/Rendering/DxAmbientLightSource.h
+++ Rldx/Rldx/Rendering/DxAmbientLightSource.h
@@ -9,40 +9,51 @@
 
 #include <string>
 
-namespace rldx {
+namespace rldx
+{
 
 	// make this base class of all buffers
 	class IBuffer : public IBindable
 	{
 		UINT m_startSlot = 0;
+
 	public:
-		void SetStartSlot(UINT startSlot) { m_startSlot = startSlot; }
+		void SetStartSlot(UINT startSlot)
+		{
+			m_startSlot = startSlot;
+		}
 	};
 
-
-	class DxTexture;	
-
-
+	class DxTexture;
 
 	/// <summary>
 	/// Sets 2/3 cubemaps needed for PBR imagebased lightng rendering
-	/// </summary>	
+	/// </summary>
 	class DxAmbientLightSource : public IBindable
 	{
-		static auto constexpr  psConstBufferName = "PS_CB:AmbientLight";
+		static auto constexpr psConstBufferName = "PS_CB:AmbientLight";
 
 		DxTexture* m_poDiffuseMap = nullptr;
 		DxTexture* m_poSpecularMap = nullptr;
 		DxTexture* m_poLUT = nullptr;
 
-		UINT m_textureStartSlot = 0;	
+		UINT m_textureStartSlot = 0;
 
 	public:
 		TDxPSShaderConstBuffer<PS_AmbientLight_Data_ConstBuffer> m_oPSConstBuffer;
-		
-		void SetStartSlot(UINT startSlot) { m_textureStartSlot = startSlot; }		
-		void SetLightRadiance(float radiance) { m_oPSConstBuffer.data.radiance = radiance;	}
-		void SetLightColor(const DirectX::XMFLOAT4& vColor = { 1,1,1,1 }) {	m_oPSConstBuffer.data.color = vColor; }
+
+		void SetStartSlot(UINT startSlot)
+		{
+			m_textureStartSlot = startSlot;
+		}
+		void SetLightRadiance(float radiance)
+		{
+			m_oPSConstBuffer.data.radiance = radiance;
+		}
+		void SetLightColor(const DirectX::XMFLOAT4& vColor = { 1, 1, 1, 1 })
+		{
+			m_oPSConstBuffer.data.color = vColor;
+		}
 
 		/// <summary>
 		/// Bind IBL const buffer, and set cubemap textures
@@ -50,45 +61,22 @@ namespace rldx {
 		/// <param name="poDC"></param>
 		void BindToDC(ID3D11DeviceContext* poDC) override;
 
-		static DxAmbientLightSource Create(
-			ID3D11Device* poDevice,
-			const std::wstring& pathDiffuseMap,
-			const std::wstring& pathSpecularMap,
-			const std::wstring& pathLUT = L"",
-			UINT startSlotConstBuf = 0,
-			UINT startSlotSRV = 0);
+		static DxAmbientLightSource Create(ID3D11Device* poDevice, const std::wstring& pathDiffuseMap,
+										   const std::wstring& pathSpecularMap, const std::wstring& pathLUT = L"",
+										   UINT startSlotConstBuf = 0, UINT startSlotSRV = 0);
 
-		static DxAmbientLightSource Create(
-			ID3D11Device* poDevice,
-			DxTexture* diffuseCubemap, 
-			DxTexture* specularCubemap, 
-			DxTexture* lut = nullptr,
-			UINT startSlotConstBuf = 0,
-			UINT startSlotSRV = 0);
+		static DxAmbientLightSource Create(ID3D11Device* poDevice, DxTexture* diffuseCubemap, DxTexture* specularCubemap,
+										   DxTexture* lut = nullptr, UINT startSlotConstBuf = 0, UINT startSlotSRV = 0);
 
-		static DxAmbientLightSource Create(
-			ID3D11Device* poDevice,
-			ByteStream& pathDiffuseMap,
-			ByteStream& pathSpecularMap,
-			ByteStream& pathLUT,
-			UINT startSlotConstBuf = 0,
-			UINT startSlotSRV = 0);
+		static DxAmbientLightSource Create(ID3D11Device* poDevice, ByteStream& pathDiffuseMap, ByteStream& pathSpecularMap,
+										   ByteStream& pathLUT, UINT startSlotConstBuf = 0, UINT startSlotSRV = 0);
 
 	private:
-		void SetTexturesFromFiles(
-			ID3D11Device* poDevice,
-			const std::wstring& pathDiffuseMap,
-			const std::wstring& pathSpecularMap,
-			const std::wstring& pathLUT = L"",
-			UINT startSlotSRV = 0
-		);
+		void SetTexturesFromFiles(ID3D11Device* poDevice, const std::wstring& pathDiffuseMap,
+								  const std::wstring& pathSpecularMap, const std::wstring& pathLUT = L"",
+								  UINT startSlotSRV = 0);
 
-		void SetTexturesFromMemory(
-			ID3D11Device* poDevice,
-			ByteStream& pathDiffuseMap,
-			ByteStream& pathSpecularMap,
-			ByteStream& pathLUT, 
-			UINT startSlotSRV = 0
-		);		
+		void SetTexturesFromMemory(ID3D11Device* poDevice, ByteStream& pathDiffuseMap, ByteStream& pathSpecularMap,
+								   ByteStream& pathLUT, UINT startSlotSRV = 0);
 	};
 }; // namespace rldx
