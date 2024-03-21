aea7b7009d0ca1a20a97389dffe50f60074ba007
mr-phazer <phazer1980@msn.com>
2024-03-21 20:08:09 +0100

    Added "mesh data name" for debugging

--- Rldx/Rldx/DataTypes/DxMeshData.h
+++ Rldx/Rldx/DataTypes/DxMeshData.h
@@ -1,12 +1,17 @@
 #pragma once
 
-#include <vector>
 #include <d3d11.h>
+#include <vector>
 #include <wrl\client.h>
 #include "..\DataTypes\CommonVertex.h"
 
-namespace rldx 
+namespace rldx
 {
+	/// <summary>
+	/// Template for storing a general CPU-side mesh
+	/// </summary>
+	/// <typeparam name="VERTEX_TYPE">Vertex type</typeparam>
+	/// <typeparam name="INDEX_TYPE">has to 2 or 4 byte uint</typeparam>
 	template <typename VERTEX_TYPE, typename INDEX_TYPE>
 	struct TRawMeshData
 	{
@@ -14,24 +19,24 @@ namespace rldx
 		std::vector<INDEX_TYPE> indices;
 	};
 
-	// TODO: clean up, remove out-commented, at the very least!
+	/// <summary>
+	/// For holding a general GPU-side mesh data, plus copy of original (for CPU-write access DX mesh buffers)
+	/// </summary>
+	/// <typeparam name="VERTEX_TYPE"></typeparam>
+	/// <typeparam name="INDEX_TYPE"></typeparam>
 	template <typename VERTEX_TYPE, typename INDEX_TYPE>
 	struct TDxMeshRenderData
 	{
-		~TDxMeshRenderData() = default;
- 
-		uint32_t                                                indexCount = 0;
-		uint32_t                                                startIndex = 0;
-		int32_t                                                 vertexOffset = 0;
-		uint32_t                                                vertexStride = sizeof(VERTEX_TYPE);
-		//DXGI_FORMAT                                             indexFormat = DXGI_FORMAT::DXGI_FORMAT_R32_UINT;
-		DXGI_FORMAT												indexFormat = sizeof(INDEX_TYPE) == 2 ? DXGI_FORMAT::DXGI_FORMAT_R16_UINT : DXGI_FORMAT::DXGI_FORMAT_R32_UINT;
-		D3D11_PRIMITIVE_TOPOLOGY								primitiveTopology = D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
-		//Microsoft::WRL::ComPtr<ID3D11InputLayout>               cpoInputLayout;
-		Microsoft::WRL::ComPtr<ID3D11Buffer>					cpoIndexBuffer;
-		Microsoft::WRL::ComPtr<ID3D11Buffer>					cpoVertexBuffer;
+		uint32_t								indexCount = 0;
+		uint32_t								startIndex = 0;
+		int32_t									vertexOffset = 0;
+		uint32_t								vertexStride = sizeof(VERTEX_TYPE);
+		DXGI_FORMAT								indexFormat = sizeof(INDEX_TYPE) == 2 ? DXGI_FORMAT::DXGI_FORMAT_R16_UINT : DXGI_FORMAT::DXGI_FORMAT_R32_UINT;
+		D3D11_PRIMITIVE_TOPOLOGY				primitiveTopology = D3D11_PRIMITIVE_TOPOLOGY::D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>	cpoIndexBuffer;
+		Microsoft::WRL::ComPtr<ID3D11Buffer>	cpoVertexBuffer;
 
-		TRawMeshData<VERTEX_TYPE, INDEX_TYPE> originalMeshData;
+		TRawMeshData<VERTEX_TYPE, INDEX_TYPE>	originalMeshData;
 	};
 
 	using DxCommonMeshData = TDxMeshRenderData<CommonVertex, uint32_t>;
