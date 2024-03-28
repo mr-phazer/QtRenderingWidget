ab9bc167173344adcbc0f90ce6a2c12a218507ce
mr-phazer <phazer1980@msn.com>
2024-03-28 20:53:42 +0100

    Minor refactoring

--- ImportExport/FileFormats/RigidModel/Creators/Vertices/VertexCreators.cpp
+++ ImportExport/FileFormats/RigidModel/Creators/Vertices/VertexCreators.cpp
@@ -1,4 +1,4 @@
-#include <Quantization\FloatCoverter.h>
+#include <Quantization\QuantTools.h>
 #include "..\..\..\..\Helpers\ByteStream.h"
 #include "..\..\RawStructs\VerticesRaw.h"
 #include "VertexCreators.h"
@@ -12,7 +12,7 @@ rldx::CommonVertex rmv2::Weighted2CommonVertexCreator::Create(ByteStream& bytes,
 
 	rldx::CommonVertex outVertex;
 
-	outVertex.position = CAVertexHelpers::DoV8PrecisionOp(FloatConverter::GetFloat4FromHalf4(rawVertex.position));
+	outVertex.position = CAVertexHelpers::DoV8PrecisionOp(quant_tools::GetFloat4FromHalf4(rawVertex.position));
 
 	// bone indices
 	outVertex.boneIndices =
@@ -23,20 +23,17 @@ rldx::CommonVertex rmv2::Weighted2CommonVertexCreator::Create(ByteStream& bytes,
 		0
 	};
 
-	// bone weights
-	// TODO: RESTOR and REMOVE DEBUGGIN CODE
-	//outVertex.weights.x = FloatConverter::GetUNORMFloatFromByte(rawVertex.boneWeights.x),
-	outVertex.weights.x = FloatConverter::GetUNormFloat(rawVertex.boneWeights.x);
-
-	outVertex.weights.y = 1.0f - outVertex.weights.x;
+	// bone weights	
+	outVertex.weights.x = quant_tools::GetUNormFloat(rawVertex.boneWeights.x);
+	outVertex.weights.y = 1.0f - outVertex.weights.x; // sum of weights must be 1.0f, and CA doesn't store weight 2
 
 	// TBN
-	outVertex.normal = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.normal);
-	outVertex.tangent = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.tangent);
-	outVertex.bitangent = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.bitangent);
+	outVertex.normal = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.normal);
+	outVertex.tangent = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.tangent);
+	outVertex.bitangent = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.bitangent);
 
 	// texture coordinates
-	outVertex.textureCoordinate = FloatConverter::GetFloat2FromHalf2(rawVertex.uv);
+	outVertex.textureCoordinate = quant_tools::GetFloat2FromHalf2(rawVertex.uv);
 
 	return outVertex;
 }
@@ -46,20 +43,20 @@ rldx::CommonVertex rmv2::DefaultCommonVertexCreator::Create(ByteStream& bytes, R
 	rldx::CommonVertex commonVertex;
 
 	// half-float to float32 for each vector coord
-	commonVertex.position = CAVertexHelpers::DoV8PrecisionOp(FloatConverter::GetFloat4FromHalf4(bytes.TReadElement<DirectX::PackedVector::XMUSHORT4>()));
+	commonVertex.position = CAVertexHelpers::DoV8PrecisionOp(quant_tools::GetFloat4FromHalf4(bytes.TReadElement<DirectX::PackedVector::XMUSHORT4>()));
 
 	// half-float to float32 for each vector coord
-	commonVertex.textureCoordinate = FloatConverter::GetFloat2FromHalf2(bytes.TReadElement<DirectX::PackedVector::XMUSHORT2>());
-	auto textureCoordinate2 = FloatConverter::GetFloat2FromHalf2(bytes.TReadElement<DirectX::PackedVector::XMUSHORT2>());
+	commonVertex.textureCoordinate = quant_tools::GetFloat2FromHalf2(bytes.TReadElement<DirectX::PackedVector::XMUSHORT2>());
+	auto textureCoordinate2 = quant_tools::GetFloat2FromHalf2(bytes.TReadElement<DirectX::PackedVector::XMUSHORT2>());
 
 	// -- For VertexFormat = "default", 'bitangent' is stored before 'tangent' and z and X are swapped in all vectors
 	// - get each vector from 4 bytes -> SNORM float4
-	commonVertex.normal = CAVertexHelpers::SwapZY(FloatConverter::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
-	commonVertex.bitangent = CAVertexHelpers::SwapZY(FloatConverter::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
-	commonVertex.tangent = CAVertexHelpers::SwapZY(FloatConverter::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
+	commonVertex.normal = CAVertexHelpers::SwapZY(quant_tools::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
+	commonVertex.bitangent = CAVertexHelpers::SwapZY(quant_tools::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
+	commonVertex.tangent = CAVertexHelpers::SwapZY(quant_tools::GetSNORMFloat3FromUByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>()));
 
 	// - get "color" from 4 bytes -> 4 UNOMRM float4
-	commonVertex.color = FloatConverter::GetUNORMFloat4FromByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>());
+	commonVertex.color = quant_tools::GetUNORMFloat4FromByte4(bytes.TReadElement<DirectX::PackedVector::XMUBYTE4>());
 
 	return commonVertex;
 }
@@ -71,13 +68,13 @@ rldx::CommonVertex rmv2::Weighted4CommonVertexCreator::Create(ByteStream& bytes,
 
 	rldx::CommonVertex outVertex;
 
-	outVertex.position = CAVertexHelpers::DoV8PrecisionOp(FloatConverter::GetFloat4FromHalf4(rawVertex.position));
+	outVertex.position = CAVertexHelpers::DoV8PrecisionOp(quant_tools::GetFloat4FromHalf4(rawVertex.position));
 
-	outVertex.normal = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.normal);
-	outVertex.tangent = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.tangent);
-	outVertex.bitangent = FloatConverter::GetSNORMFloat3FromUByte4(rawVertex.bitangent);
+	outVertex.normal = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.normal);
+	outVertex.tangent = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.tangent);
+	outVertex.bitangent = quant_tools::GetSNORMFloat3FromUByte4(rawVertex.bitangent);
 
-	outVertex.textureCoordinate = FloatConverter::GetFloat2FromHalf2(rawVertex.uv);
+	outVertex.textureCoordinate = quant_tools::GetFloat2FromHalf2(rawVertex.uv);
 
 	// bone indices
 	outVertex.boneIndices =
@@ -88,15 +85,15 @@ rldx::CommonVertex rmv2::Weighted4CommonVertexCreator::Create(ByteStream& bytes,
 		rawVertex.boneIndices.w,
 	};
 
-	outVertex.weights.x = FloatConverter::GetUNORMFloatFromByte(rawVertex.boneWeights.x);
-	outVertex.weights.y = FloatConverter::GetUNORMFloatFromByte(rawVertex.boneWeights.y);
-	outVertex.weights.z = FloatConverter::GetUNORMFloatFromByte(rawVertex.boneWeights.z);
+	outVertex.weights.x = quant_tools::GetUNORMFloatFromByte(rawVertex.boneWeights.x);
+	outVertex.weights.y = quant_tools::GetUNORMFloatFromByte(rawVertex.boneWeights.y);
+	outVertex.weights.z = quant_tools::GetUNORMFloatFromByte(rawVertex.boneWeights.z);
 	outVertex.weights.w = 1.0f - (outVertex.weights.x + outVertex.weights.y + outVertex.weights.z); // quick-dirty normalization
 
 
 	if (rmv2Version == Rmv2VersionEnum::RMV2_V8)
 	{
-		outVertex.color = FloatConverter::GetUNORMFloat4FromByte4(rawVertex.color);
+		outVertex.color = quant_tools::GetUNORMFloat4FromByte4(rawVertex.color);
 	}
 
 	return outVertex;
