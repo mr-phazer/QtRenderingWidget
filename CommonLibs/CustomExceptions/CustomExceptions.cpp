b87505dfc260c5c302918285b5071ae319e3a30d
mr-phazer <phazer1980@msn.com>
2024-05-28 01:46:42 +0200

    Added needed custom exception

--- CommonLibs/CustomExceptions/CustomExceptions.cpp
+++ CommonLibs/CustomExceptions/CustomExceptions.cpp
@@ -11,4 +11,15 @@ const char* ConLogException::what() const throw()
 {
 	return m_message.c_str();
 }
-;
+
+// --------------------------------------------------------------------------------
+
+ConLogExceptionVerbose::ConLogExceptionVerbose(const std::string& message) : m_message(FULL_FUNC_INFO(message))
+{
+	logging::LogActionError("Exception: " + message);
+}
+
+const char* ConLogExceptionVerbose::what() const throw()
+{
+	return m_message.c_str();
+}
--- CommonLibs/CustomExceptions/CustomExceptions.h
+++ CommonLibs/CustomExceptions/CustomExceptions.h
@@ -11,3 +11,13 @@ public:
 	ConLogException(const std::string& message);
 	virtual const char* what() const throw();
 };
+
+
+class ConLogExceptionVerbose : public std::exception
+{
+	std::string m_message;
+
+public:
+	ConLogExceptionVerbose(const std::string& message);
+	virtual const char* what() const throw();
+};
