d5d2b82edd0186fc10358b46f50c17675caff77f
mr-phazer <phazer1980@msn.com>
2024-06-25 12:25:55 +0200

    Updates high-res timer

--- CommonLibs/Timer/SystemClockChecker.h
+++ CommonLibs/Timer/SystemClockChecker.h
@@ -8,48 +8,48 @@ namespace timer
 	/// <summary>
 		/// Uses the CPUs high resolution clock, to count time intervals
 		/// </summary>
-	class SystemClockChecker
+class SystemClockChecker
+{
+	typedef std::chrono::high_resolution_clock Time;
+
+public:
+	SystemClockChecker()
 	{
-		typedef std::chrono::high_resolution_clock Time;
+		ResetLocalTime();
+	}
 
-	public:
-		SystemClockChecker()
-		{
-			ResetLocalTime();
-		}
+	/// <summary>
+	/// Resets the local timer to "now"
+	/// </summary>
+	void ResetLocalTime()
+	{
+		m_startTime = std::chrono::high_resolution_clock::now();
+	}
 
-		/// <summary>
-		/// Resets the local timer to "now"
-		/// </summary>
-		void ResetLocalTime()
-		{
-			m_startTime = std::chrono::high_resolution_clock::now();
-		}
+	/// <summary>
+	///  Get "local" time, 
+	/// </summary>
+	/// <returns>"state time" - "now" </returns>
+	float GetLocalTime() const
+	{
+		auto timeElapsed = std::chrono::high_resolution_clock::now();
 
-		/// <summary>
-		///  Get "local" time, 
-		/// </summary>
-		/// <returns>"state time" - "now" </returns>
-		float GetLocalTime() const
-		{
-			auto timeElapsed = std::chrono::high_resolution_clock::now();
+		auto value = std::chrono::duration<float, std::chrono::seconds::period>(timeElapsed - m_startTime);
 
-			auto value = std::chrono::duration<float, std::chrono::seconds::period>(timeElapsed - m_startTime);
+		float retValue = value.count();
 
-			float retValue = value.count();
+		return retValue;
+	}
 
-			return retValue;
-		}
+	/// <summary>
+	/// Rerturn the CPU "ticks"
+	/// </summary>		
+	long long GetSystemTick() const
+	{
+		return ::std::chrono::high_resolution_clock::now().time_since_epoch().count();
+	}
 
-		/// <summary>
-		/// Rerturn the CPU "ticks"
-		/// </summary>		
-		long long GetSystemTick() const
-		{
-			return ::std::chrono::high_resolution_clock::now().time_since_epoch().count();
-		}
-
-	private:
-		::std::chrono::steady_clock::time_point m_startTime;
-	};
+private:
+	::std::chrono::steady_clock::time_point m_startTime;
+};
 }
